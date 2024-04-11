db23ba0bf440868e6cffebb474dc2dc0bdc12944
ayushvarshn <43218356+ayushvarshn@users.noreply.github.com>
2024-04-12 01:00:30 +0530

    Create pandas.py

---
+++ pandas.py
@@ -0,0 +1,4 @@
+merge, concat and join difference?
+join(): it combines two DataFrames by index.
+merge(): it combines two DataFrames by the column or columns you specify.
+concat(): it combines two or more DataFrames vertically or horizontally.
